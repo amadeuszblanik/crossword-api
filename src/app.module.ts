2858a6594aa3f07fa73655f1a3b18145cc092c2b
Amadeusz Blanik <amadeusz@blanik.me>
2022-02-08 19:33:38 +0100

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { WordsController } from './words/words.controller';
+import { WordsService } from './words/words.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController, WordsController],
+  providers: [AppService, WordsService],
+})
+export class AppModule {}
